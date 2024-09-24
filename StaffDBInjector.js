f96e2aefc96170b1fcadcb972197477bb9b81877
Nicolas <nicmatt@uw.edu>
2024-09-24 17:42:11 -0400

    added staffdbinjector.js and updated database setup

---
+++ StaffDBInjector.js
@@ -0,0 +1,21 @@
+/* Import Statements */
+const mysql = require('./connectDatabase');
+const bcrypt = require('bcrypt');
+
+
+/* Creating main method only to have an async function wrapping
+the await function
+ */
+
+async function main() {
+    // Establish database connection first
+    const connection = await mysql.connectDatabase();
+    const passwordToInsert = await bcrypt.hash("12345", 10);
+    const insertStatement = `INSERT INTO Staff (staff_number, name, email, password) VALUES (?, ?, ?, ?)`;
+
+    const [result] = await connection.execute(insertStatement, [1, "nicmatt", "nicmatt@uw.edu", passwordToInsert]);
+
+    console.log(result);
+}
+
+main();
