510641463b2bd4782b2d94c8bb01e7c83e21ee0b
Nicolas <nicmatt@uw.edu>
2024-09-29 07:44:06 -0400

    changed cors origin to github page

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5500",
+    origin: "https://nicmatt-15.github.io",
     methods: ["GET", "POST"]
   }
 });
