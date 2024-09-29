5ce657cb6261d67600526ed12992477b29a8a8dd
Nicolas <nicmatt@uw.edu>
2024-09-29 04:56:47 -0400

    replaced bcrypt with bcryptjs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('./connectDatabase');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const http = require('http');
 const { Server } = require('socket.io'); // imports Server class
 const { start } = require('repl');
